a8f582352e307803e36dda05d6a927a0ae79be5d
vasudevshreekumar <vasudevshreekumar@gmail.com>
2026-08-22 17:03:55 +0530

    setup changed

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ sudo apt update
 sudo apt install nftables
 sudo systemctl enable nftables
 sudo systemctl start nftables
-sudo nft add table inet firedome_firewall
+sudo nft add table inet firedome-w
 
 #chains
-sudo nft add chain inet firewall input '{ type filter hook input priority 0; policy accept; }'
-sudo nft add chain inet firewall output '{ type filter hook output priority 0; policy accept; }'
-sudo nft add chain inet firewall forward '{ type filter hook forward priority 0; policy accept; }'
+sudo nft add chain inet firedome-w input '{ type filter hook input priority 0; policy accept; }'
+sudo nft add chain inet firedome-w output '{ type filter hook output priority 0; policy accept; }'
+sudo nft add chain inet firedome-w forward '{ type filter hook forward priority 0; policy accept; }'
